19a1d0fc51026d8c9b90993d1ec6ff989ca1b97e
Raghukul-r <111770075+Raghukul-r@users.noreply.github.com>
2024-03-26 07:24:48 +0530

    add files

---
+++ basic_practice/string.js
@@ -0,0 +1,19 @@
+let string1 = 'Raghukul raman ';
+string2 = " ayansh rathod"
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+// console.log(string1 + string2)
+// console.log(string1.split(" " ))
+// console.log(sentence.charAt(4));
+// console.log(sentence.toUpperCase());
+// console.log(sentence.length)
+
+let str = "     hello world  "
+// console.log(str.trim()) // remove extra spaces
+
+//console.log(string1.concat(string2));
+
+const newString = string1.substring(0, 6)
+//console.log(newString);
+
+console.log(sentence.includes('quick'))  // if given string is avaliable then it returns true .
+
